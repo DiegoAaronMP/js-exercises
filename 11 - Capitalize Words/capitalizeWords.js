38a7e8c62ea7bf5b7ae40f85960aa923e1eb3f2e
Diego <daaron.macias@gmail.com>
2025-04-30 18:17:23 -0600

    Exercise 11

---
+++ 11 - Capitalize Words/capitalizeWords.js
@@ -0,0 +1,39 @@
+/**
+ * Exercise 11: Capitalize Words
+ * 
+   Description:
+   Write a function that takes a string and returns the same string but 
+   with the first letter of each word capitalized.
+   Words are separated by spaces. You should preserve spacing, 
+   including multiple or leading/trailing spaces.
+
+   Examples:
+   capitalizeWords("hello world")          // "Hello World"
+   capitalizeWords("javaScript is fun")    // "JavaScript Is Fun"
+   capitalizeWords("")                     // ""
+   capitalizeWords("  multiple   spaces ") // "  Multiple   Spaces "
+ */
+
+const capitalizeWords = (string) => {
+    if (string.length === 0 ) {
+        return '';
+    }
+
+    let newString = '';
+
+    for (let i = 0; i < string.length; i++) {
+        if (/([a-z])/g.test(string[i]) && (!/([a-z])/g.test(string[i - 1]) || !string[i - 1])) {
+            newString += string[i].toUpperCase()
+        } else {
+            newString += string[i]
+        }
+    }
+
+    return newString;
+}
+
+console.log(capitalizeWords("hello world"));
+console.log(capitalizeWords("javaScript is fun"));
+console.log(capitalizeWords(""));
+console.log(capitalizeWords("  multiple   spaces "));
+
